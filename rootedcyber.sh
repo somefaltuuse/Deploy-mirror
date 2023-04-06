7a35a5d41e6a2d313e37790302cb29a39ef86778
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2023-04-06 21:57:20 +0530

    Update rootedcyber.sh

--- rootedcyber.sh
+++ rootedcyber.sh
@@ -1,9 +1,9 @@
 git clone your fork repo
 cd Deploy*
 if [ -e config.env ];then
+printf "\n Please wait for installing \n\n"
 pip3 install --no-cache-dir -r requirements.txt > /dev/null 2>&1
 bash start.sh
 else
 printf "\n\n\n First Upload your config.env\n\n"
-exit
 fi
