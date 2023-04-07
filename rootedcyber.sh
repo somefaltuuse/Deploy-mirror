09ecf895409f329e6b447dcf4faa058d001533b8
somefaltuuse <128957438+somefaltuuse@users.noreply.github.com>
2023-04-07 08:57:36 +0530

    Update rootedcyber.sh

--- rootedcyber.sh
+++ rootedcyber.sh
@@ -1,9 +1,15 @@
-git clone your fork repo
+gt() {
+git clone https://github.com/somefaltuuse/Deploy-mirror
+}
+gt > /dev/null 2>&1
 cd Deploy*
 if [ -e config.env ];then
 printf "\n Please wait for installing \n\n"
-pip3 install --no-cache-dir -r requirements.txt > /dev/null 2>&1
+pi() {
+pip3 install --no-cache-dir -r requirements.txt
 bash start.sh
+}
+pi > /dev/null 2>&1
 else
 printf "\n\n\n First Upload your config.env\n\n"
 fi
