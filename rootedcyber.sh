c06cdd6fa0b5ff86a6e1240459033d5b1f03f66c
somefaltuuse <128957438+somefaltuuse@users.noreply.github.com>
2023-04-07 09:18:02 +0530

    Update rootedcyber.sh

--- rootedcyber.sh
+++ rootedcyber.sh
@@ -6,10 +6,11 @@ cd Deploy*
 if [ -e config.env ];then
 printf "\n Please wait for installing \n\n"
 pi() {
-pip3 install --no-cache-dir -r requirements.txt
+pip3 install --no-cache-dir -r requirements.txt > /dev/null 2>&1
+printf "\n Bot Starting !!! \n"
 bash start.sh
 }
-pi > /dev/null 2>&1
+pi
 else
 printf "\n\n\n First Upload your config.env\n\n"
 fi
