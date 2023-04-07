12f808a777cac367548efb2f9e19ef97e6fdc75d
somefaltuuse <128957438+somefaltuuse@users.noreply.github.com>
2023-04-07 16:09:16 +0530

    Update rootedcyber.sh

--- rootedcyber.sh
+++ rootedcyber.sh
@@ -1,18 +1,13 @@
 gt() {
-git clone 
+git clone https://github.com/somefaltuuse/Deploy-mirror
 }
 gt > /dev/null 2>&1
 cd Deploy*
-if [ -e config.env ];then
 printf "\n Please wait for installing \n\n"
 pi() {
 pip install --upgrade pip
-pip3 install --no-cache-dir -r requirements.txt > /dev/null 2>&1
+pip3 install --no-cache-dir -r requirements.txt
+}
+pi > /dev/null 2>&1
 printf "\n Bot Starting !!! \n"
 python3 -m bot
-pip install pyrogram
-}
-pi
-else
-printf "\n\n\n First Upload your config.env\n\n"
-fi
